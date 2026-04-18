b462d1fc44fe82b92942f526277df34f3a7fbae6
Ryan-gosling0 <ahmednajjar2ti2@gmail.com>
2026-04-18 10:40:53 +0100

    app building

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "my-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/icon.png"
+];
+
+// Install → cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch → serve from cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
